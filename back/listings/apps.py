0f44031baee1b3f1dc54c530d039e3af22b16365
frank c. <frankcayaco@gmail.com>
2026-03-03 11:18:57 +0800

    signal compares to engine's estimate

--- back/listings/apps.py
+++ back/listings/apps.py
@@ -4,3 +4,8 @@ from django.apps import AppConfig
 class ListingsConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'listings'
+
+    def ready(self):
+        # connect signals
+        from . import signals  # noqa: F401
+
